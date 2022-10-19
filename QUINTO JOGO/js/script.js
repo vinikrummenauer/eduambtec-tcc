4ec7803e634fb9682f485eb8f21ce37dd2ba4043
Vinicius Krummenauer <viniciuskdepaula@gmail.com>
2022-10-19 14:51:31 -0300

    final

--- QUINTO JOGO/js/script.js
+++
@@ -1,5 +0,0 @@
-ScrollReveal().reveal('.col-12.col-md-10.offset-md-1', {delay: 200});
-
-ScrollReveal().reveal('#featured-container', {delay: 200});
-
-ScrollReveal().reveal('#info-container', {delay: 200});
